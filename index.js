6f11ebb2ec21e633456adab9bb5922bc8e14039f
Jonathan Hosein <jonathan.hosein@live.com>
2018-05-31 23:05:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function theBeatlesPlay(arrayMusicians, arrayInstruments) {
 
 function johnLennonFacts(factsArr) {
   let i = 0;
-  
+
   while (i < factsArr.length) {
 
     factsArr[i] += " !!!";
